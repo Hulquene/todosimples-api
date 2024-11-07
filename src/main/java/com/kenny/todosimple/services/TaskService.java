4cc4f3b7f895c47a5997a3ffe392092b5a08fa4a
Hulquene <hulquener@gmail.com>
2024-11-07 16:11:55 +0000

    Verificar erro na task Service

--- src/main/java/com/kenny/todosimple/services/TaskService.java
+++ src/main/java/com/kenny/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.kenny.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.kenny.todosimple.models.Task;
 import com.kenny.todosimple.models.User;
 import com.kenny.todosimple.repositories.TaskRepository;
-import com.kenny.todosimple.repositories.UserRepository;
 
 public class TaskService {
 
   @Autowired
   private TaskRepository taskRepository;
-  @Autowired
-  private UserService userService;
+  // @Autowired
+  // private UserService userService;
 
   public Task findById(Long id) {
     Optional<Task> task = this.taskRepository.findById(id);
@@ -23,25 +23,31 @@ public class TaskService {
         .orElseThrow(() -> new RuntimeException("Tarefa nao encontrado! " + id + ", Tipo:" + User.class.getName()));
   }
 
-  @Transactional
-  public Task create(Task obj) {
-    User user = this.userService.findById(obj.getUser().getId());
-    obj.setId(null);
-    obj.setUser(user);
-    obj = this.taskRepository.save(obj);
-    return obj;
+  public List<Task> findAllByUserId(Long userId) {
+    List<Task> task = this.taskRepository.findByUser_Id(userId);
+    return task;
   }
 
-  @Transactional
-  public Task update(Task obj) {
-    Task newObj = findById(obj.getId());
-    newObj.setDescription(obj.getDescription());
-    return this.taskRepository.save(newObj);
-  }
+  // @Transactional
+  // public Task create(Task obj) {
+  //   User user = this.userService.findById(obj.getUser().getId());
+  //   obj.setId(null);
+  //   obj.setUser(user);
+  //   obj = this.taskRepository.save(obj);
+  //   return obj;
+  // }
 
-  public void delete(Long idd) {
-    findById(idd);
-    this.taskRepository.deleteById(idd);
-  }
+  // @Transactional
+  // public Task update(Task obj) {
+  //   Task newObj = findById(obj.getId());
+  //   newObj.setDescription(obj.getDescription());
+  //   return this.taskRepository.save(newObj);
+  // }
+
+
+  // public void delete(Long idd) {
+  //   findById(idd);
+  //   this.taskRepository.deleteById(idd);
+  // }
 
 }
